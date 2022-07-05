f0744f3f86bc6174e601e3946eb6da96f549530e
zengchaowu <root@zengchaowu.com>
2022-07-05 22:40:12 +0800

    chore: add script decrypt

--- bin/doraemon.js
+++ bin/doraemon.js
@@ -10,8 +10,19 @@ program
   .description("nuxt tools")
   .argument("<add/remove>", "action")
   .argument("<name>", "name")
-  .action((action,name) => {
+  .action((action, name) => {
     import(`../lib/nuxt-${name}/index.js`).then((result) => {
+      result.default[action]();
+    });
+  });
+
+program
+  .command("script")
+  .description("scripts")
+  .argument("<name>", "name")
+  .argument("<action>", "action")
+  .action((name, action) => {
+    import(`../lib/script-${name}/index.js`).then((result) => {
       result.default[action]();
     });
   });
