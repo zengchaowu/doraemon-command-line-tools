9d91e4b12e26f36e9c4cb96efad80df7c46ebe43
zengchaowu <root@zengchaowu.com>
2022-07-08 16:05:34 +0800

    chore: 修改router

--- lib/nuxt-layouts/lib/default.vue
+++ lib/nuxt-layouts/lib/default.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="absolute inset-0 flex">
+    <div class="flex-grow relative">
+      <a-config-provider :locale="locale">
+        <div class="absolute inset-0 flex">
+          <div class="flex-grow flex">
+            <IndexAside
+              class="flex-none no-scrollbar overflow-y-auto shadow-md bg-white z-20"
+            />
+            <div class="flex-grow flex flex-col">
+              <div class="flex-grow overflow-y-auto relative">
+                <div class="absolute inset-0 flex">
+                  <Nuxt />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a-config-provider>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import zhCN from "ant-design-vue/lib/locale-provider/zh_CN";
+export default {
+  data() {
+    return {
+      locale: zhCN,
+    };
+  },
+};
 </script>
