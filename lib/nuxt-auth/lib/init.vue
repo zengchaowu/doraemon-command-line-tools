cdd97ad141aa87d3f02e5272f1467b0b46833bd2
zengchaowu <root@zengchaowu.com>
2022-07-20 10:27:52 +0800

    chore: stash

--- lib/nuxt-auth/lib/init.vue
+++ lib/nuxt-auth/lib/init.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="flex flex-col gap-2">
-    <a-spin />
-    <span>初始化中</span>
-  </div>
+  <span>初始化中</span>
 </template>
 
 <script>
-import useAuthStore from "@/pinia/auth";
+import getMenu from "@/network/http/getMenu";
+// import createMenu from '@/functions/createMenu'
 import useApplicationStore from "@/pinia/application";
+
+function close(children) {
+  for (const child of children) {
+    if (child.children) {
+      child.isOpen = false;
+      close(child.children);
+    }
+  }
+}
+
 export default {
   name: "InitPage",
   init: true,
   layout: "empty",
-  mounted() {
-    const authStore = useAuthStore();
-    authStore.load();
-    const applicationStore = useApplicationStore();
-    applicationStore.isReady = true;
-    const redirect = applicationStore.redirect;
-    applicationStore.redirect = undefined;
-    this.$router.push(redirect ?? "/");
+  async mounted() {
+    try {
+      const result = await this.$request(getMenu());
+      // const result = createMenu()
+      for (const child of result) {
+        child.path && (child.icon = child.path);
+      }
+      close(result);
+      const applicationStore = useApplicationStore();
+      applicationStore.menu = {
+        isOpen: true,
+        children: result,
+      };
+      applicationStore.isReady = true;
+      const redirect = applicationStore.redirect;
+      applicationStore.redirect = undefined;
+      this.$router.push(redirect ?? "/");
+    } catch (error) {}
   },
 };
 </script>
